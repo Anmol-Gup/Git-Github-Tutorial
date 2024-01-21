b67313555bb36243d2c1a61a7bed26b7ef6cd643
Anmol-Gup <sp430661@gmail.com>
2024-01-21 19:34:30 +0530

    add isEligible()

--- index.js
+++ index.js
@@ -7,3 +7,10 @@ function add(a,b){
 function showName(name){
     console.log(`My name is ${name}.`)
 }
+
+function isEligible(age){
+    if(age>=18)
+        console.log(`Eligible`)
+    else
+        console.log('Not eligible')
+}
